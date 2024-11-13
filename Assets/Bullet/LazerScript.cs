28fe42d9df952cc24dc82103409d7df646224d74
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-11-14 05:04:24 +0900

    色々追加

--- Assets/Bullet/LazerScript.cs
+++ Assets/Bullet/LazerScript.cs
@@ -9,7 +9,9 @@ public class LazerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Destroy(gameObject, 0.1f);
+        float moveSpeed = 27.0f;
+        rb.velocity = new Vector3(0, 0, moveSpeed);
+        Destroy(gameObject, 3);
     }
 
     // Update is called once per frame
@@ -17,12 +19,12 @@ public class LazerScript : MonoBehaviour
     {
 
     }
-    //void OnCollisionEnter(Collision other)
-    //{
-    //    if (other.gameObject.tag == "Enemy")
-    //    {
-    //        Destroy(this.gameObject);
-    //    }
-    //}
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
 }
